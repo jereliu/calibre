89d7c9e40db85c0826b987f96c83b7f99efadf6b
Jeremiah Zhe Liu <zhel90@gmail.com>
2018-09-24 21:12:08 -0400

    additional option for 1d data generation

--- calibre/util/data.py
+++ calibre/util/data.py
@@ -6,14 +6,16 @@ def sin_curve_1d(x):
     return x + np.sin(4 * x) + np.sin(13 * x)
 
 
-def generate_1d_data(N, f, noise_sd=0.03, seed=None):
+def generate_1d_data(N, f, noise_sd=0.03, seed=None, uniform_x=False):
     """Generate 1D regression data in Louizos and Welling (2016)"""
     np.random.seed(seed)
 
-    x = np.concatenate([
-        np.random.uniform(low=0, high=0.6, size=int(N * 0.8)),
-        np.random.uniform(low=0.8, high=1, size=N - int(N * 0.8))])
-    # x = np.random.uniform(low=0, high=1, size=N)
+    if uniform_x:
+        x = np.random.uniform(low=0, high=1, size=N)
+    else:
+        x = np.concatenate([
+            np.random.uniform(low=0, high=0.6, size=int(N * 0.8)),
+            np.random.uniform(low=0.8, high=1, size=N - int(N * 0.8))])
     eps = np.random.normal(loc=0, scale=noise_sd, size=N)
     y = f(x + eps)
     return x, y
