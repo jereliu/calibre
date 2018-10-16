5148ca4bac313bc7dbdbd2d0aac271fff79281ba
Jeremiah Zhe Liu <zhel90@gmail.com>
2018-10-16 09:15:24 -0400

    misc fix

--- example/calibre_1d_flat.py
+++ example/calibre_1d_flat.py
@@ -32,7 +32,7 @@ import seaborn as sns
 tfd = tfp.distributions
 
 DEFAULT_LS_WEIGHT = 0.2
-DEFAULT_LS_RESID = 0.3
+DEFAULT_LS_RESID = 0.2
 _SAVE_ADDR_PREFIX = "./result/calibre_1d_flat"
 _FIT_BASE_MODELS = False
 
@@ -41,14 +41,14 @@ _FIT_BASE_MODELS = False
 """""""""""""""""""""""""""""""""
 
 N_train = 20
-N_test = 50
+N_test = 20
 N_valid = 500
 
 X_train, y_train = generate_1d_data(N=N_train, f=sin_curve_1d,
                                     noise_sd=0.03, seed=1000,
                                     uniform_x=True)
 X_test, y_test = generate_1d_data(N=N_test, f=sin_curve_1d,
-                                  noise_sd=0.03, seed=2000,
+                                  noise_sd=0.03, seed=1500,
                                   uniform_x=True)
 
 X_train = np.expand_dims(X_train, 1).astype(np.float32)
@@ -58,7 +58,7 @@ y_test = y_test.astype(np.float32)
 
 std_y_train = np.std(y_train)
 
-X_valid = np.expand_dims(np.linspace(-1, 2, N_valid), 1).astype(np.float32)
+X_valid = np.expand_dims(np.linspace(-0.5, 1.5, N_valid), 1).astype(np.float32)
 y_valid = sin_curve_1d(X_valid)
 
 N, D = X_train.shape
@@ -76,7 +76,7 @@ if _FIT_BASE_MODELS:
                        X_test, y_test,
                        X_valid, y_valid,
                        kern_func_dict=DEFAULT_KERN_FUNC_DICT,
-                       n_valid_sample=5000,
+                       n_valid_sample=500,
                        save_addr_prefix="{}/base".format(_SAVE_ADDR_PREFIX))
 
 """""""""""""""""""""""""""""""""
@@ -88,10 +88,12 @@ with open(os.path.join(_SAVE_ADDR_PREFIX, 'base/base_test_pred.pkl'), 'rb') as f
 with open(os.path.join(_SAVE_ADDR_PREFIX, 'base/base_valid_pred.pkl'), 'rb') as file:
     base_valid_pred = pk.load(file)
 
+model_names = ["rbf_0.2", "rbf_0.05", "rbf_0.01",
+               "rquad1_0.1", "rquad1_0.2", "rquad1_0.5"]
 base_test_pred = {key: value for key, value in base_test_pred.items() if
-                  ('rbf' in key)}
+                  (key in model_names)}
 base_valid_pred = {key: value for key, value in base_valid_pred.items()
-                   if key in list(base_test_pred.keys())}
+                   if key in model_names}
 
 """2.1. sampler basic config"""
 N = X_test.shape[0]
